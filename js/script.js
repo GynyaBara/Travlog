1522e64a02016f2423eededc78b00100491af421
GynyaBara <102617101+GynyaBara@users.noreply.github.com>
2023-09-13 20:35:34 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: 2.2,
+  spaceBetween: 20,
+
+  // If we need pagination
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
